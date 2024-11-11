f1cdb05f1fd3eef5fd44244474c4451634675481
Alex Rusin <alexrusin_2000@yahoo.com>
2024-11-11 07:12:55 -0800

    add jwt authentication

--- src/routes/v1/tasks/controller.ts
+++ src/routes/v1/tasks/controller.ts
@@ -3,7 +3,11 @@ import EntityNotFoundError from "../../../errors/EntityNotFoundError";
 import prisma from "../../../prisma-client";
 
 export const listTasks = async (req: Request, res: Response) => {
-  const tasks = await prisma.task.findMany();
+  const tasks = await prisma.task.findMany({
+    where: {
+      user_id: req.auth?.payload.sub,
+    },
+  });
   res.status(200).json({ tasks });
 };
 
@@ -11,6 +15,7 @@ export const getTask = async (req: Request, res: Response) => {
   const task = await prisma.task.findUnique({
     where: {
       id: req.params.id,
+      user_id: req.auth?.payload.sub,
     },
   });
   if (!task) {
